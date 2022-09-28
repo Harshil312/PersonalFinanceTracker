31b20e5d60434c7c133bbe2af0e3d009d005c892
Harshil312 <harshilparmar812@gmail.com>
2022-09-29 00:41:25 +0530

    Main Activity and Themes updated

--- app/src/main/java/com/example/personalfinancetracking/MainActivity.kt
+++ app/src/main/java/com/example/personalfinancetracking/MainActivity.kt
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         Handler(Looper.getMainLooper()).postDelayed(Runnable {
             startActivity(Intent(this,LoginActivity::class.java))
         },3000)
