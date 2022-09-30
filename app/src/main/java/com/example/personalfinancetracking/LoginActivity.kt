fca3a3763f7cd6003ab6845e97c3794c0c8a9876
Harshil312 <harshilparmar812@gmail.com>
2022-09-30 15:00:16 +0530

    Minute Changes

--- app/src/main/java/com/example/personalfinancetracking/LoginActivity.kt
+++ app/src/main/java/com/example/personalfinancetracking/LoginActivity.kt
@@ -2,10 +2,21 @@ package com.example.personalfinancetracking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val username = findViewById<TextInputEditText>(R.id.UsernameET_Login)
+        val password = findViewById<TextInputEditText>(R.id.PasswordET_Login)
+        val loginBtn = findViewById<Button>(R.id.btnLogin)
+
+        loginBtn.setOnClickListener {
+            var unm = username.text.toString()
+            var pass = password.text.toString()
+        }
     }
 }
