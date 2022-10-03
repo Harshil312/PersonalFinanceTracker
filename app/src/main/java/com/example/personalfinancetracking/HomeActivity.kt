d12324f0c6c433a39ce38d79b9282e5e979f118e
Harshil312 <harshilparmar812@gmail.com>
2022-10-03 23:20:52 +0530

    BottomNavigation Bond

--- app/src/main/java/com/example/personalfinancetracking/HomeActivity.kt
+++ app/src/main/java/com/example/personalfinancetracking/HomeActivity.kt
@@ -3,7 +3,11 @@ package com.example.personalfinancetracking
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.personalfinancetracking.databinding.ActivityHomeBinding
 import com.example.personalfinancetracking.databinding.ActivityLoginBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -12,17 +16,24 @@ class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
     private lateinit var firebaseAuth: FirebaseAuth
-
+    lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_home)
 
         firebaseAuth = FirebaseAuth.getInstance()
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.NavHostFragment) as NavHostFragment
+
+        navController = navHostFragment.navController
+        binding.bottomNavBar.setupWithNavController(navController)
 //        binding.btnLogout.setOnClickListener {
 //
 //            firebaseAuth.signOut()
 //            startActivity(Intent(this,LoginActivity::class.java))
 //            finish()
 //        }
+
     }
 }
