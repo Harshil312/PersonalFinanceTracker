349e168a0c3e50f495b3fa6e2e84abc4ed5ab437
Harshil312 <harshilparmar812@gmail.com>
2022-10-03 11:46:49 +0530

    Google Authentication Configured

--- app/src/main/java/com/example/personalfinancetracking/LoginActivity.kt
+++ app/src/main/java/com/example/personalfinancetracking/LoginActivity.kt
@@ -5,24 +5,54 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import com.example.personalfinancetracking.databinding.ActivityLoginBinding
 import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val username = findViewById<TextInputEditText>(R.id.EmailET_Login)
-        val password = findViewById<TextInputEditText>(R.id.PasswordET_Login)
-        val loginBtn = findViewById<Button>(R.id.btnLogin)
-        val LoginTextView = findViewById<TextView>(R.id.TextView_Login)
-
-        loginBtn.setOnClickListener {
-            var unm = username.text.toString()
-            var pass = password.text.toString()
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.TextViewLogin.setOnClickListener {
+            var intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
         }
-        LoginTextView.setOnClickListener {
-            var intent = Intent(this,RegisterActivity::class.java)
+
+        binding.btnLogin.setOnClickListener {
+            val email = binding.EmailETLogin.text.toString()
+            val pass = binding.PasswordETLogin.text.toString()
+
+
+            if (email.isNotEmpty() && pass.isNotEmpty()) {
+
+                firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, HomeActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+            } else {
+                Toast.makeText(this, "Empty Fields are Not Allowed!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (firebaseAuth.currentUser != null){
+            val intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
         }
     }
--- app/src/main/java/com/example/personalfinancetracking/RegisterActivity.kt
+++ app/src/main/java/com/example/personalfinancetracking/RegisterActivity.kt
@@ -4,17 +4,50 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
+import com.example.personalfinancetracking.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
 
-        var LoginTextView =findViewById<TextView>(R.id.TextView_Register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        LoginTextView.setOnClickListener {
-            var intent = Intent(this,LoginActivity::class.java)
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.TextViewRegister.setOnClickListener {
+            var intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+
+        binding.btnRegister.setOnClickListener {
+            val email = binding.EmailETRegister.text.toString()
+            val pass = binding.PasswordETRegister.text.toString()
+            val confirmPass = binding.RePasswordETRegister.text.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()) {
+                if (pass == confirmPass) {
+                    firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            val intent = Intent(this, LoginActivity::class.java)
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                } else {
+                    Toast.makeText(this, "Password Does Not Match!", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "Empty Fields are Not Allowed!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
     }
 }
