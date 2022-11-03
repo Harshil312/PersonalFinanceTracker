4781b6256ecb628c52ddfd5467fe86773b888ca0
ankulbbcspl <ankulparmar.bbcspl@gmail.com>
2022-11-03 17:10:27 +0530

    AnalyticFragment.kt PieChart Completed

--- app/src/main/java/com/example/personalfinancetracking/database/Query.kt
+++ app/src/main/java/com/example/personalfinancetracking/database/Query.kt
@@ -3,68 +3,115 @@ package com.example.personalfinancetracking.database
 import android.util.Log
 
 class Query {
-    lateinit var qry:String
+    lateinit var qry: String
     val TAG = "Query"
 
-//    fun getLabs():String
+    //    fun getLabs():String
 //    {
 //        qry = "SELECT LabName FROM LabMaster"
 //        Log.d(TAG, "getLabs : $qry")
 //        return qry
 //    }
 //
-    fun getIncomeData():String
-    {
+    fun getIncomeData(): String {
         qry = "SELECT IncomeId,Amount,IncomeType,Date,Month,Note FROM Income"
         Log.d(TAG, "getIncomeData : $qry")
         return qry
     }
 
-    fun getExpenseData():String
-    {
+    fun getExpenseData(): String {
         qry = "SELECT ExpenseId,Amount,ExpenseType,Date,Month,Note FROM Expense"
         Log.d(TAG, "getExpenseData : $qry")
         return qry
     }
 
-    fun getTotalIncome():String
-    {
+    fun getTotalIncome(): String {
         qry = "SELECT SUM(Amount) FROM Income"
         Log.d(TAG, "getTotalIncomeSaving : $qry")
         return qry
     }
-    fun getTotalExpense():String
-    {
+
+    fun getTotalExpense(): String {
         qry = "SELECT SUM(Amount) FROM Expense"
         Log.d(TAG, "getTotalExpense : $qry")
         return qry
     }
 
-    fun getTotalSaving():String
-    {
+    fun getTotalSaving(): String {
         qry = "SELECT SUM(Amount) FROM Income WHERE IncomeType = 'Saving'"
         Log.d(TAG, "getTotalSaving : $qry")
         return qry
     }
 
-    fun getTotalSalary():String
-    {
+    fun getTotalSalary(): String {
         qry = "SELECT SUM(Amount) FROM Income WHERE IncomeType = 'Salary'"
         Log.d(TAG, "getTotalSalary : $qry")
         return qry
     }
 
-    fun getTotalInvestmentReturn():String
-    {
+    fun getTotalInvestmentReturn(): String {
         qry = "SELECT SUM(Amount) FROM Income WHERE IncomeType = 'Investment Return'"
         Log.d(TAG, "getTotalInvestmentReturn : $qry")
         return qry
     }
 
-    fun getTotalOtherIncome():String
-    {
+    fun getTotalOtherIncome(): String {
         qry = "SELECT SUM(Amount) FROM Income WHERE IncomeType = 'Other'"
         Log.d(TAG, "getTotalOtherIncome : $qry")
+        return qry
+    }
+
+    fun getTotalGrocery(): String {
+        qry = "SELECT SUM(Amount) FROM Expense WHERE ExpenseType = 'Grocery'"
+        Log.d(TAG, "getTotalGrocery : $qry")
+        return qry
+    }
+
+    fun getTotalShopping(): String {
+        qry = "SELECT SUM(Amount) FROM Expense WHERE ExpenseType = 'Shopping'"
+        Log.d(TAG, "getTotalShopping : $qry")
+        return qry
+    }
+
+    fun getTotalInvestment(): String {
+        qry = "SELECT SUM(Amount) FROM Expense WHERE ExpenseType = 'Investment'"
+        Log.d(TAG, "getTotalInvestment : $qry")
+        return qry
+    }
+
+    fun getTotalBillPayments(): String {
+        qry = "SELECT SUM(Amount) FROM Expense WHERE ExpenseType = 'Bill Payments'"
+        Log.d(TAG, "getTotalBillPayments : $qry")
+        return qry
+    }
+
+    fun getTotalFeePayments(): String {
+        qry = "SELECT SUM(Amount) FROM Expense WHERE ExpenseType = 'Fee Payments'"
+        Log.d(TAG, "getTotalFeePayments : $qry")
+        return qry
+    }
+
+    fun getTotalInsurance(): String {
+        qry = "SELECT SUM(Amount) FROM Expense WHERE ExpenseType = 'Insurance'"
+        Log.d(TAG, "getTotalInsurance : $qry")
+        return qry
+    }
+
+    fun getTotalRent(): String {
+        qry = "SELECT SUM(Amount) FROM Expense WHERE ExpenseType = 'Rent'"
+        Log.d(TAG, "getTotalRent : $qry")
+        return qry
+    }
+
+    fun getTotalFoodExpense(): String {
+        qry = "SELECT SUM(Amount) FROM Expense WHERE ExpenseType = 'Food Expense'"
+        Log.d(TAG, "getTotalFoodExpense : $qry")
+        return qry
+    }
+
+    fun getTotalOther(): String {
+        qry = "SELECT SUM(Amount) FROM Expense WHERE ExpenseType = 'Other'"
+        Log.d(TAG, "getTotalOtherExpense : $qry")
         return qry
     }
 }
--- app/src/main/java/com/example/personalfinancetracking/fragment/AnalyticFragment.kt
+++ app/src/main/java/com/example/personalfinancetracking/fragment/AnalyticFragment.kt
@@ -1,5 +1,6 @@
 package com.example.personalfinancetracking.fragment
 
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -12,6 +13,7 @@ import com.example.personalfinancetracking.R
 import com.example.personalfinancetracking.database.DatabaseManager
 import com.example.personalfinancetracking.database.Query
 import com.example.personalfinancetracking.databinding.FragmentAnalyticBinding
+import org.eazegraph.lib.models.PieModel
 
 
 class AnalyticFragment : Fragment() {
@@ -34,6 +36,7 @@ class AnalyticFragment : Fragment() {
         binding.tvExpenseAnalytics.text = getTotalExpense().toString()
         binding.tvIncomeAnalytics.text = getTotalIncome().toString()
         binding.tvSavingAnalytics.text = (getTotalIncome() - getTotalExpense()).toString()
+        showPieChart()
         return binding.root
     }
 
@@ -73,15 +76,153 @@ class AnalyticFragment : Fragment() {
         return count
     }
 
-    fun getTotal():Int{
+    fun getTotalSalary():Int{
         var count = 0
         try {
-            count = databaseManager.ExecuteScalar(Query().getTotalExpense())!!.toInt()
+            count = databaseManager.ExecuteScalar(Query().getTotalSalary())!!.toInt()
         }catch (e:Exception)
         {
             e.printStackTrace()
-            Log.e(TAG, "getTotalIncome: "+e.message)
+            Log.e(TAG, "getTotalSalary: "+e.message)
         }
         return count
     }
+
+    fun getTotalInvestmentReturn():Int{
+        var count = 0
+        try {
+            count = databaseManager.ExecuteScalar(Query().getTotalInvestmentReturn())!!.toInt()
+        }catch (e:Exception)
+        {
+            e.printStackTrace()
+            Log.e(TAG, "getTotalInvestmentReturn: "+e.message)
+        }
+        return count
+    }
+    fun getTotalOtherIncome():Int{
+        var count = 0
+        try {
+            count = databaseManager.ExecuteScalar(Query().getTotalOtherIncome())!!.toInt()
+        }catch (e:Exception)
+        {
+            e.printStackTrace()
+            Log.e(TAG, "getTotalOtherIncome: "+e.message)
+        }
+        return count
+    }
+    fun getTotalGrocery():Int{
+        var count = 0
+        try {
+            count = databaseManager.ExecuteScalar(Query().getTotalGrocery())!!.toInt()
+        }catch (e:Exception)
+        {
+            e.printStackTrace()
+            Log.e(TAG, "getTotalGrocery: "+e.message)
+        }
+        return count
+    }
+    fun getTotalShopping():Int{
+        var count = 0
+        try {
+            count = databaseManager.ExecuteScalar(Query().getTotalShopping())!!.toInt()
+        }catch (e:Exception)
+        {
+            e.printStackTrace()
+            Log.e(TAG, "getTotalShopping: "+e.message)
+        }
+        return count
+    }
+    fun getTotalInvestment():Int{
+        var count = 0
+        try {
+            count = databaseManager.ExecuteScalar(Query().getTotalInvestment())!!.toInt()
+        }catch (e:Exception)
+        {
+            e.printStackTrace()
+            Log.e(TAG, "getTotalInvestment: "+e.message)
+        }
+        return count
+    }
+    fun getTotalBillPayments():Int{
+        var count = 0
+        try {
+            count = databaseManager.ExecuteScalar(Query().getTotalBillPayments())!!.toInt()
+        }catch (e:Exception)
+        {
+            e.printStackTrace()
+            Log.e(TAG, "getTotalBillPayments: "+e.message)
+        }
+        return count
+    }
+    fun getTotalFeePayments():Int{
+        var count = 0
+        try {
+            count = databaseManager.ExecuteScalar(Query().getTotalFeePayments())!!.toInt()
+        }catch (e:Exception)
+        {
+            e.printStackTrace()
+            Log.e(TAG, "getTotalFeePayments: "+e.message)
+        }
+        return count
+    }
+    fun getTotalInsurance():Int{
+        var count = 0
+        try {
+            count = databaseManager.ExecuteScalar(Query().getTotalInsurance())!!.toInt()
+        }catch (e:Exception)
+        {
+            e.printStackTrace()
+            Log.e(TAG, "getTotalInsurance: "+e.message)
+        }
+        return count
+    }
+    fun getTotalRent():Int{
+        var count = 0
+        try {
+            count = databaseManager.ExecuteScalar(Query().getTotalRent())!!.toInt()
+        }catch (e:Exception)
+        {
+            e.printStackTrace()
+            Log.e(TAG, "getTotalRent: "+e.message)
+        }
+        return count
+    }
+    fun getTotalFoodExpense():Int{
+        var count = 0
+        try {
+            count = databaseManager.ExecuteScalar(Query().getTotalFoodExpense())!!.toInt()
+        }catch (e:Exception)
+        {
+            e.printStackTrace()
+            Log.e(TAG, "getTotalFoodExpense: "+e.message)
+        }
+        return count
+    }
+    fun getTotalOtherExpense():Int{
+        var count = 0
+        try {
+            count = databaseManager.ExecuteScalar(Query().getTotalOther())!!.toInt()
+        }catch (e:Exception)
+        {
+            e.printStackTrace()
+            Log.e(TAG, "getTotalOtherExpense: "+e.message)
+        }
+        return count
+    }
+    fun showPieChart()
+    {
+        binding.piechart.addPieSlice(PieModel("Grocery", getTotalGrocery().toFloat(), Color.parseColor("#FE6DA8")))
+        binding.piechart.addPieSlice(PieModel("Shopping", getTotalShopping().toFloat(), Color.parseColor("#56B7F1")))
+        binding.piechart.addPieSlice(PieModel("Investment", getTotalInvestment().toFloat(), Color.parseColor("#5645FF")))
+        binding.piechart.addPieSlice(PieModel("Bill Payments", getTotalBillPayments().toFloat(), Color.parseColor("#545712")))
+        binding.piechart.addPieSlice(PieModel("Fee Payments", getTotalFeePayments().toFloat(), Color.parseColor("#555787")))
+        binding.piechart.addPieSlice(PieModel("Insurance", getTotalInsurance().toFloat(), Color.parseColor("#5FD765")))
+        binding.piechart.addPieSlice(PieModel("Rent", getTotalRent().toFloat(), Color.parseColor("#56E6BA")))
+        binding.piechart.addPieSlice(PieModel("Food Expense", getTotalFoodExpense().toFloat(), Color.parseColor("#56FC5F")))
+        binding.piechart.addPieSlice(PieModel("Salary", getTotalSalary().toFloat(), Color.parseColor("#564BD5")))
+        binding.piechart.addPieSlice(PieModel("Investment Return", getTotalInvestmentReturn().toFloat(), Color.parseColor("#56D9D5")))
+        binding.piechart.addPieSlice(PieModel("Other Income", getTotalOtherIncome().toFloat(), Color.parseColor("#564B12")))
+        binding.piechart.addPieSlice(PieModel("Other", getTotalOtherExpense().toFloat(), Color.parseColor("#564A96")))
+        binding.piechart.startAnimation()
+   }
 }
